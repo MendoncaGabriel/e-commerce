7bab62986e0524f780a42426a77ff6d3273cc933
Seu Nome <MendoncaGabriel1997@gmail.com>
2024-04-12 16:57:04 -0400

    add

--- database/database.js
+++ database/database.js
@@ -4,10 +4,10 @@ require('dotenv').config()
 
 
 const conexao = mysql.createConnection({
-    host: process.env.LOCALADDRESS,
-    user: process.env.USER,
-    password: process.env.PASS,
-    database: process.env.DATABASE,
+    host: process.env.LOCALADDRESS || 'database-gam.c3gcao0wi23m.us-east-1.rds.amazonaws.com',
+    user: process.env.USER || 'gabroviski',
+    password: process.env.PASS || 'Gam1997Vsc',
+    database: process.env.DATABASE || 'shopdosbaloes',
     port: 3306 
   });
    
